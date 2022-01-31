4f768de3b2cdf3e9c98c79902602397e59d38061
Kundan Kumar <kundanvrs@gmail.com>
2022-01-31 21:34:44 +0530

    profile

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Kundan Kumar',
     title: "Python Developer | DevOps Engineer",
     desciption:" a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://github.com/kundanvrs/raw-data/releases/download/test/profileImage.png',
+    image: 'https://github.com/kundanvrs/raw-data/releases/download/test/kkImageProfile.jpg',
     resumePdf: resume
 }
